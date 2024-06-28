f3caf6cb3f33207f7aa39976eee3f0d2e2f2d9ea
Interstellar2424 <lakshyaahirr@gmail.com>
2024-06-28 14:24:03 +0530

    Update Calculator.py
    Added two more operations in calculator

--- First Git/Calculator.py
+++ First Git/Calculator.py
@@ -1,13 +1,15 @@
 #Calculator: A Project by Lakshya;
 
+import math
 print("Calculator: A Project by Lakshya")
+print("if your operation is sqrt then the second value will be ignored.")
 
 #Input the first and second int.
 a = input("Input the First integer: ")
 b = input("Input the Second integer: ")
 
 #Defining the operations (+,-,*,/,%);
-operation = input("Define the Operation: ")
+operation = input("Define the Operation(+,-,*,/,%,^,sqrt:")
 
 #conditioning and calculating with first and second Integer by Implementing the the 5 Operations (+,-,*,/,%);
 if operation == "+":
@@ -23,7 +25,13 @@ elif operation == "/":
     print(int(a) / int(b))
 
 elif operation == "%":
-    print(int(a) / int(b))
+    print(int(a) % int(b))
+
+elif operation == "^":
+    print(int(a) ^ int(b))
+
+elif operation == "sqrt":
+    print(math.sqrt(int(a)))
 
 else:
     print("The Calculation is Undefined")
